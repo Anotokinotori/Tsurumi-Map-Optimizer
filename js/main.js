3d507d776da3d82b0e24d3fe497236dcce780a9d
Anotokinotori <ousuke80@gmail.com>
2025-09-27 21:35:18 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,8 +38,9 @@ const TsurumiApp = {
         // Buttons
         this.elements.goToCurrentBtn = document.getElementById('go-to-current-btn');
         this.elements.guideBtn = document.getElementById('guide-btn');
-        this.elements.tsurumiInfoBtn = document.getElementById('tsurumi-info-btn'); // Added
-        this.elements.cycleHoldInfoBtn = document.getElementById('cycle-hold-info-btn'); // Added
+        this.elements.headerTsurumiInfoBtn = document.getElementById('header-tsurumi-info-btn');
+        this.elements.startTsurumiInfoBtn = document.getElementById('start-tsurumi-info-btn');
+        this.elements.cycleHoldInfoBtn = document.getElementById('cycle-hold-info-btn');
         this.elements.disclaimerLink = document.getElementById('disclaimer-link');
         this.elements.disclaimerLinkResultPC = document.getElementById('disclaimer-link-result-pc');
         this.elements.disclaimerLinkResultMobile = document.getElementById('disclaimer-link-result-mobile');
@@ -130,8 +131,9 @@ const TsurumiApp = {
 
         // Modals
         this.elements.guideBtn.addEventListener('click', () => this.ui.showModal('guide-modal'));
-        this.elements.tsurumiInfoBtn.addEventListener('click', () => this.ui.showModal('tsurumi-info-modal')); // Added
-        this.elements.cycleHoldInfoBtn.addEventListener('click', () => this.ui.showModal('cycle-hold-info-modal')); // Added
+        this.elements.headerTsurumiInfoBtn.addEventListener('click', () => this.ui.showModal('tsurumi-info-modal'));
+        this.elements.startTsurumiInfoBtn.addEventListener('click', () => this.ui.showModal('tsurumi-info-modal'));
+        this.elements.cycleHoldInfoBtn.addEventListener('click', () => this.ui.showModal('cycle-hold-info-modal'));
         this.elements.disclaimerLink.addEventListener('click', () => this.ui.showModal('disclaimer-modal'));
         this.elements.disclaimerLinkResultPC.addEventListener('click', () => this.ui.showModal('disclaimer-modal'));
         this.elements.disclaimerLinkResultMobile.addEventListener('click', () => this.ui.showModal('disclaimer-modal'));
@@ -831,3 +833,4 @@ const PlanCalculator = {
 
 // --- APP START ---
 document.addEventListener('DOMContentLoaded', () => TsurumiApp.init());
+
